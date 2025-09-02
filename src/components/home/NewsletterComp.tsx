8e0127783bbd0673eb36ae900e7fa75653c56940
iysaofficial <youngscientist.iysa@gmail.com>
2025-09-02 11:20:02 +0700

    newsletter

--- src/components/home/NewsletterComp.tsx
+++ src/components/home/NewsletterComp.tsx
@@ -1,19 +1,54 @@
-const NewsletterComp = () =>{
-    return(
+"use client";
+import React, { useEffect } from "react";
+
+
+const NewsletterComp: React.FC = () => {
+  useEffect(() => {
+    const scriptURL =
+      "https://script.google.com/macros/s/AKfycbwfw1dAhzyY6p6crVCCuoEXzb_E7hBOi8cYuIBu_-JZzz2a25h-tCvT6Xd3dUH9Mx2fQA/exec";
+
+    const form = document.forms.namedItem("regist-form");
+
+    if (form) {
+      const handleSubmit = async (e: Event) => {
+        e.preventDefault();
+        try {
+          await fetch(scriptURL, {
+            method: "POST",
+            body: new FormData(form),
+          });
+          alert("Data berhasil dikirim!");
+          form.reset();
+        } catch (error) {
+          console.error("Error:", error);
+          alert("Gagal mengirim data.");
+        }
+      };
+
+      form.addEventListener("submit", handleSubmit);
+
+      // cleanup listener
+      return () => {
+        form.removeEventListener("submit", handleSubmit);
+      };
+    }
+  }, []);
+
+  return(
         <>
         <section className="newsletter-section">
             <div className="newsletter-container">
                 <div className="newsletter-box">
                     <h2>Subscribe to <br /> Our Newsletter</h2>
-                    <form action="https://formspree.io/f/xoqgjrok" method="POST">
-                        <input type="email" placeholder="Masukan Email Anda" name="email"/>
-                        <input type="submit" value={"Subscribe"} />
+                    <form action="https://formspree.io/f/xoqgjrok" method="POST" name="regist-form">
+                        <input type="email" placeholder="Masukan Email Anda" name="Email"/>
+                        <button type="submit" className="submit-button">Subscribe</button>
                     </form>
                 </div>
             </div>
         </section>
         </>
     )
-}
+};
 
-export default NewsletterComp
+export default NewsletterComp;
